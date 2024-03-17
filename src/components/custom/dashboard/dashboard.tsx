6d5275a5d21cd81266985b35a9a8e4d8e9b8f9a1
thepriyansh01 <priyansh.m.2002@gmail.com>
2024-03-17 23:28:25 +0530

    added resume
    Signed-off-by: thepriyansh01 <priyansh.m.2002@gmail.com>

--- src/components/custom/dashboard/dashboard.tsx
+++ src/components/custom/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button'
-import { DiscordLogoIcon, GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon, TextAlignLeftIcon, TwitterLogoIcon } from '@radix-ui/react-icons'
-import React from 'react'
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { DiscordLogoIcon, GitHubLogoIcon, InstagramLogoIcon, LinkedInLogoIcon, TextAlignLeftIcon, TwitterLogoIcon } from '@radix-ui/react-icons';
 import { Github, HardDriveDownload, Mail, Phone, Send } from 'lucide-react';
 
 const Dashboard = () => {
@@ -15,39 +15,39 @@ const Dashboard = () => {
             <div className='text-md md:text-xl text-center'>
                 Welcome to my portfolio! I&apos;m a dedicated full-stack developer with a strong expertise in web development. I specialize in building dynamic and intuitive web applications.
             </div>
-            <div className="flex flex-wrap justify-center items-center gap-8 my-3">
-                <a href="https://github.com/thepriyansh01" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
+            <div className="flex flex-wrap justify-center items-center gap-8 px-4 my-3">
+                <a href="https://github.com/thepriyansh01" target="_blank" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
                     <GitHubLogoIcon height='40' width="40" />
                 </a>
-                <a href="https://www.linkedin.com/in/priyansh-sahu-ab9380202/" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
+                <a href="https://www.linkedin.com/in/priyansh-sahu-ab9380202/" target="_blank" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
                     <LinkedInLogoIcon height='40' width="40" />
                 </a>
-                <a href="https://twitter.com/_thepriyansh01" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
+                <a href="https://twitter.com/_thepriyansh01" target="_blank" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
                     <TwitterLogoIcon height='40' width="40" />
                 </a>
-                <a href="https://www.instagram.com/_thepriyansh/" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
+                <a href="https://www.instagram.com/_thepriyansh/" target="_blank" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
                     <InstagramLogoIcon height='40' width="40" />
                 </a>
-                <a href="https://discord.gg/SRZYjv3Q" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
+                <a href="https://discord.gg/SRZYjv3Q" target="_blank" className='hover:rotate-[360deg] duration-700 hover:text-yellow-500'>
                     <DiscordLogoIcon height='40' width="40" />
                 </a>
-                <a href="mailto:priyansh.sahu.cer21@iitbhu.ac.in" className="hover:rotate-[360deg] duration-700 hover:text-yellow-500">
+                <a href="mailto:priyansh.sahu.cer21@iitbhu.ac.in" target="_blank" className="hover:rotate-[360deg] duration-700 hover:text-yellow-500">
                     <Mail height={40} width={40} />
                 </a>
 
             </div>
-            <div className="flex flex-wrap justify-center items-center gap-5 md:gap-10 font-semibold md:text-2xl">
-                <a href='mailto:priyansh.sahu.cer21@iitbhu.ac.in' className='flex justify-center items-center gap-3 px-4 md:px-8 py-2 rounded-[var(--radius)] btn-gradient border-2 shadow-lg shadow-black/25 hover:scale-110 duration-300 hover:-rotate-1'>
+            <div className="flex flex-wrap justify-center items-center w-full px-4 gap-5 md:gap-10 font-semibold text-2xl">
+                <a href='mailto:priyansh.sahu.cer21@iitbhu.ac.in' target="_blank" className='flex justify-center items-center gap-3 w-full sm:w-max px-8 py-2 rounded-[var(--radius)] btn-gradient border-2 shadow-lg shadow-black/25 hover:scale-110 duration-300 hover:-rotate-1'>
                     <span>Contact Me</span>
                     <Send size={25} />
                 </a>
-                <button className='flex justify-center items-center gap-3  px-4 md:px-8 py-2 rounded-[var(--radius)] bg-secondary border-2 shadow-lg shadow-black/25 hover:scale-110 duration-300 hover:rotate-1'>
+                <a href='https://drive.google.com/file/d/1LmbYn9rovs5e7BffkpEhK_jnAZPV82d7/view' target="_blank" className='flex justify-center items-center gap-3 w-full sm:w-max px-8 py-2 rounded-[var(--radius)] bg-secondary border-2 shadow-lg shadow-black/25 hover:scale-110 duration-300 hover:rotate-1'>
                     <span>Resume</span>
                     <HardDriveDownload size={25} />
-                </button>
+                </a>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
