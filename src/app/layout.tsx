91369ac32003d4c9631647bbff92d6be7616bda6
thepriyansh01 <priyansh.m.2002@gmail.com>
2024-03-23 12:16:53 +0530

    added next-theme
    Signed-off-by: thepriyansh01 <priyansh.m.2002@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
+
 import Header from "@/components/custom/navBars/header";
-import "./globals.css";
 import Footer from "@/components/custom/navBars/footer";
 
 const nunito = Nunito({ subsets: ["latin"] });
@@ -17,16 +19,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={`${nunito.className} dark select-none`}>
-        <Header />
-        <div className="flex items-center justify-center w-full min-h-screen">
-          <div className="h-full w-full">
-            {children}
-          </ div>
-        </div>
-        <Footer />
+      <body className={`${nunito.className} select-none`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <div className="flex items-center justify-center w-full min-h-screen">
+            <div className="h-full w-full">
+              {children}
+            </ div>
+          </div>
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
