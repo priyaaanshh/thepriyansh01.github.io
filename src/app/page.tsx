5eb739f15e829c1ee638645ee99c18ec231fb98e
thepriyansh01 <priyansh.m.2002@gmail.com>
2024-03-27 23:24:39 +0530

    Removed snapy effect from mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ import Heading from '@/components/custom/heading/heading'
 
 const Home = () => {
   return (
-    <div className='snap-y snap-mandatory flex flex-col items-center justify-start h-screen w-full px-4 overflow-y-scroll no-scrollbar'>
-      <div className="snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-center items-center gap-5">
+    <div className='sm:snap-y snap-mandatory flex flex-col items-center justify-start h-screen w-full px-4 pb-3 overflow-y-scroll no-scrollbar'>
+      <div className="sm:snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-center items-center gap-5">
         <Dashboard />
       </div>
-      <div className="snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-center items-center gap-5">
+      <div className="sm:snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-center items-center gap-5">
         <div className="my-5"></div>
         <Heading >{"About Me"}</Heading>
         <About />
       </div>
-      <div className="snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-center items-center gap-5">
+      <div className="sm:snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-center items-center gap-5">
         <div className="my-5"></div>
         <Heading >{"Skills"}</Heading>
         <Skills />
       </div>
-      <div className="snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-start items-center gap-5">
+      <div className="sm:snap-start shrink-0 min-h-screen w-full max-w-[850px] flex flex-col justify-start items-center gap-5">
         <div className="my-5"></div>
         <Heading >{'Projects'}</Heading>
         <Projects projectsToShow={3} />
