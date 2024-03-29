4aeace833c0c293f5dae6d5af5b9ab351ad7d9e6
thepriyansh01 <priyansh.m.2002@gmail.com>
2024-03-29 11:35:30 +0530

    removed blog page
    Signed-off-by: thepriyansh01 <priyansh.m.2002@gmail.com>

--- src/components/custom/navBars/header.tsx
+++ src/components/custom/navBars/header.tsx
@@ -1,11 +1,6 @@
 "use client";
 import React from 'react'
 import Link from 'next/link';
-import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import { Button } from '@/components/ui/button';
-import { HamburgerMenuIcon } from '@radix-ui/react-icons';
-import Logo from '../../../assets/images/Logo.jpg'
-import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 const Header = () => {
@@ -29,12 +24,6 @@ const Header = () => {
                     </div>
                 </Link>
 
-                <Link href='/blogs' passHref>
-                    <div className={`flex justify-center items-center font-semibold md:px-4 md:text-lg px-4 py-1 ${isActive('/blogs')}`}>
-                        Blogs
-                    </div>
-                </Link>
-
                 <Link href='/experience' passHref>
                     <div className={`flex justify-center items-center font-semibold md:px-4 md:text-lg px-4 py-1 ${isActive('/experience')}`}>
                         Experience
@@ -47,41 +36,8 @@ const Header = () => {
                     </div>
                 </Link>
             </div>
-            {/* <div className='flex justify-center items-center my-5 bg-gradient text-secondary-foreground backdrop-blur rounded-[var(--radius)] px-4 py-2 sm:text-3xl cursor-pointer select-none'>Website in devlopment phase</div> */}
         </div>
     )
 }
 
 export default Header
-
-
-
-// < div className = 'flex lg:hidden' >
-//     <Sheet>
-//         <SheetTrigger asChild>
-//             <Button variant="ghost" size='icon'><HamburgerMenuIcon /></Button>
-//         </SheetTrigger>
-//         <SheetContent>
-//             <SheetHeader>
-//                 <SheetClose asChild>
-//                     <Link href='/' className="flex justify-center items-center overflow-hidden">
-//                         <Image src={Logo} alt='' className='size-32 object-contain' />
-//                     </Link>
-//                 </SheetClose>
-//                 <SheetTitle>
-//                     <SheetClose asChild>
-//                         <Link href='/' className='text-2xl font-semibold tracking-tight'>{name}</Link>
-//                     </SheetClose>
-//                 </SheetTitle>
-//                 <SheetDescription>
-//                     Celebrating 100 years of Ceramic Engineering IIT BHU
-//                 </SheetDescription>
-//             </SheetHeader>
-//             <div className="flex flex-col gap-3 mt-5">
-//                 <SheetClose asChild>
-//                     <Link href='/' className="cursor-pointer hover:bg-primary hover:dark:text-secondary rounded-lg duration-300 px-4 py-2" >Home</Link>
-//                 </SheetClose>
-//             </div>
-//         </SheetContent>
-//     </Sheet>
-//             </div >
