22dee82645f4b4ea5185ee0d3e7cc31a83de53cf
priyaaanshh <priyansh.m.2002@gmail.com>
2024-04-07 16:41:20 +0530

    updated experience

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,11 +1,19 @@
-import TimeLine from "@/components/custom/timeline/timeline";
 import React from "react";
 import timeLineData from "./timelineData";
+
+import TimeLine from "@/components/custom/timeline/timeline";
+import MainHeading from "@/components/custom/heading/mainheading";
+import SubHeading from "@/components/custom/heading/subheading";
+
 const Experience = () => {
   return (
-    <div className="flex flex-col justify-start items-start lg:items-center h-screen w-full overflow-x-hidden">
-      <div className="my-28 px-6">
-        <TimeLine events={timeLineData} />
+    <div className="relative grid place-items-center w-full h-screen overflow-x-hidden overflow-y-scroll no-scrollbar">
+      <div className="flex flex-col justify-center items-center mt-20 w-full max-w-[850px] px-8">
+        <MainHeading>My Experiences</MainHeading>
+        <SubHeading>Want to connect? My inbox is always open!</SubHeading>
+        <div className="flex w-full justify-start lg:justify-center">
+          <TimeLine events={timeLineData} />
+        </div>
       </div>
     </div>
   );
